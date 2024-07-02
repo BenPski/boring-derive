6846c3d50a31dc92c3185bebf1002a4f595387f0
Ben Pski <ben@pepski.com>
2024-07-02 16:41:32 -0500

    docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,127 @@
+//! Derive macros for some common patterns
+//!
+//! The currently implemented patterns are:
+//!  - Builder
+//!  - From
+//!
+//! # Builder
+//! for the `Builder` macro it generates an impl with methods of the form:
+//! ```
+//! fn field(mut self, value: impl Into<Type>) -> Self {
+//!     self.field = value.into()
+//!     self
+//! }
+//! ```
+//!
+//! An example of the generated code for a struct is:
+//! ```
+//! #[derive(Default, Builder)]
+//! struct Example {
+//!     item: String,
+//!     value: usize,
+//! }
+//!
+//! // generated impl
+//! impl Example {
+//!     fn item(mut self, value: impl Into<String>) -> Self {
+//!         self.item = value.into();
+//!         self
+//!     }
+//!
+//!     fn value(mut self, value: impl Into<usize>) -> Self {
+//!         self.value = value.into();
+//!         self
+//!     }
+//! }
+//!
+//! // using the values
+//! fn main() {
+//!     let ex = Example::default()
+//!         .item("something")
+//!         .value(1);
+//!     ...
+//! }
+//! ```
+//!
+//! if you want to not include a field in the builder pattern use the `skip` attribute:
+//! ```
+//! #[derive(Builder)]
+//! struct Example {
+//!     #[builder(skip)]
+//!     item: String,
+//!     value: usize,
+//! }
+//! ```
+//!
+//! if you do not want to have the `Into` use the `no_into` attribute:
+//! ```
+//! #[derive(Builder)]
+//! struct Example {
+//!     #[builder(no_into)]
+//!     item: String,
+//!     value: usize,
+//! }
+//! ```
+//!
+//! The Builder pattern is not defined for enums, unit-like struct, newtypes, and tuple structs
+//!
+//! # From
+//! For the `From` derive it implements the trivial `From<Type>` implementations:
+//! ```
+//! #[derive(From)]
+//! enum Example {
+//!     Empty,
+//!     Number(f32),
+//!     Pair(String, String),
+//! }
+//!
+//! // will generate
+//! impl From<()> for Example {
+//!     fn from(value: ()) -> Self {
+//!         Example::Empty
+//!     }
+//! }
+//! impl From<f32> for Example {
+//!     fn from(value: f32) -> Self {
+//!         Example::Number(f32)
+//!     }
+//! }
+//! impl From<(String, String)> for Example {
+//!     fn from(value: (String, String)) -> Self {
+//!         Example::Pair(value)
+//!     }
+//! }
+//! ```
+//!
+//! For struct datatypes it uses tuples as the type to convert from:
+//! ```
+//! #[derive(From)]
+//! struct Example {
+//!     item: usize
+//!     value: String
+//! }
+//!
+//! // generates
+//! impl From<(usize, String)> for Example {
+//!     fn from(value: (usize, String)) -> Self {
+//!         Example {
+//!             item: value.0,
+//!             value: value.1,
+//!         }
+//!     }
+//! }
+//! ```
+//!
+//! If you need to not generate a `From` implementation use the `skip` attribute
+//! ```
+//! #[derive(From)]
+//! enum Example {
+//!     #[from(skip)]
+//!     Empty,
+//!     Number(f32),
+//!     Pair(String, String),
+//! }
+//! ```
 mod builder_derive;
 mod core;
 mod from_derive;
